8aa25a60c7f3127fddedb0565f64e0a9756b394b
Kimberlyn Stoddard <kimberlynrstoddard@gmail.com>
2020-07-16 15:18:54 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,30 +1,33 @@
 def find_item_by_name_in_collection(name, collection)
- count = 0 
- while count < collection.length 
- if collection[count][:item] == name 
-   return collection[count]
- end 
- count += 1 
- end 
- end 
+  item = nil
+collection.each do |each_hash|
+  each_hash.each do |key, value|
+    if value == name 
+      item = {key => value}
+    end
+  end
+end
+item
+end 
 
-def consolidate_cart(cart)
- new_cart = []
- count = 0 
- while count < cart.length 
- new_item = find_item_by_name_in_collection(cart[count][:item], new_cart)
- if new_item
-   new_item[:count] += 1 
- else 
-   new_item = {
-     :item => cart[count][:item],
-     :price => cart[count][:price],
-     :clearance => cart[count][:clearance],
-     :count => 1 
-   }
-   new_cart << new_item 
- end 
- count =+ 1 
- end 
- new_cart
- end 
+def consolidate_cart(unconsolidated_cart)
+  new_cart = []
+  unconsolidated_cart.each do |hash|
+    hash.each do |key, value|
+      matching_value = find_item_by_name_in_collection(value, new_cart)
+      if !matching_value
+        new_cart << hash 
+        new_cart[-1][:count] = 1 
+      else
+        new_cart.each do |new_hash|
+          new_hash.each do |new_key, new_value|
+            if new_value == value 
+              new_hash[:count] += 1 
+            end
+          end
+        end
+      end
+    end
+  end
+  new_cart
+end 
